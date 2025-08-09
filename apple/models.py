33c317e759aa855f9d58b71b8b8eedf5cf6bb2b5
pramod-7-paudel <paudelpramod654@gmail.com>
2025-08-09 21:57:26 +0545

    changed time zone of published date

--- apple/models.py
+++ apple/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.utils import timezone # import timezone for date handling
 
 class Blogbpost(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
-    published= models.DateTimeField(auto_now_add=True)
-
+    published_date = models.DateTimeField(default=timezone.now)  # auto set to now 
+    author = models.CharField(max_length=100,default='pramod')
     def __str__(self):
         return self.title
 
--- apple/views.py
+++ apple/views.py
@@ -2,17 +2,23 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from .models import Blogbpost
 from django.core.serializers import serialize
+from django.utils import timezone
 import json
 
 def home(request):
     return render(request, 'task/post_list.html')
 
 def get_posts(request):
-    posts = Blogbpost.objects.all().order_by('-published')
+    posts = Blogbpost.objects.all().order_by('-published_date')
+
     data = [
-        {'title': post.title,
+        {
+            'title': post.title,
             'content': post.content,
-            'published': post.published.strftime('%Y-%m-%d %H:%M:%S'),
-        } for post in posts 
+            'author': post.author,
+            'published': timezone.localtime(post.published_date).strftime('%Y-%m-%d %H:%M:%S')
+                if post.published_date else "Unknown date",
+        }
+        for post in posts
     ]
     return JsonResponse(data, safe=False)
